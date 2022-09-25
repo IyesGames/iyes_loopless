1e1d74ce1d2934eca40e4a21b7da4cd4cafcfafa
Ida Iyes <identry@protonmail.com>
2022-09-25 14:48:47 +0300

    fix builds with different combinations of cargo features
    Closes #37

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,9 @@ pub mod prelude {
     pub use crate::condition::{ConditionHelpers, IntoConditionalSystem, ConditionSet, AddConditionalToSet};
     #[cfg(feature = "fixedtimestep")]
     pub use crate::fixedtimestep::{FixedTimestepInfo, FixedTimestepStage};
-    #[cfg(feature = "app")]
+    #[cfg(all(feature = "states", feature = "app"))]
     pub use crate::state::app::AppLooplessStateExt;
+    #[cfg(all(feature = "states", feature = "app"))]
     pub use crate::state::schedule::ScheduleLooplessStateExt;
     #[cfg(feature = "states")]
     pub use crate::state::{CurrentState, NextState, StateTransitionStage};
