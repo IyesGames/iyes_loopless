952cb605c450ec4433b8fcdb40980945ac1e68b3
Ida Iyes <identry@protonmail.com>
2022-10-14 17:43:07 +0300

    prelude: make sure all app/schedule extension traits are correct

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,18 @@ pub mod state;
 
 pub mod prelude {
     pub use crate::condition::{ConditionHelpers, IntoConditionalSystem, ConditionSet, AddConditionalToSet};
+
     #[cfg(feature = "fixedtimestep")]
     pub use crate::fixedtimestep::{FixedTimestepInfo, FixedTimestepStage};
-    #[cfg(all(feature = "states", feature = "app"))]
-    pub use crate::state::app::AppLooplessStateExt;
-    #[cfg(all(feature = "states", feature = "app"))]
-    pub use crate::state::schedule::ScheduleLooplessStateExt;
+    #[cfg(feature = "fixedtimestep")]
+    pub use crate::fixedtimestep::schedule::ScheduleLooplessFixedTimestepExt;
+    #[cfg(all(feature = "fixedtimestep", feature = "app"))]
+    pub use crate::fixedtimestep::app::AppLooplessFixedTimestepExt;
+
     #[cfg(feature = "states")]
     pub use crate::state::{CurrentState, NextState, StateTransitionStage};
+    #[cfg(feature = "states")]
+    pub use crate::state::schedule::ScheduleLooplessStateExt;
+    #[cfg(all(feature = "states", feature = "app"))]
+    pub use crate::state::app::AppLooplessStateExt;
 }
