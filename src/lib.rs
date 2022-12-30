502db18da058caa39c14d41248ebf309c46af4fb
emerald <emerald_actual@proton.me>
2022-12-30 03:48:21 -0500

    add WaitingState struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! Composable Alternatives to Bevy's RunCriteria, States, FixedTimestep
-//! 
+//!
 //! This crate offers alternatives to the Run Criteria, States, and FixedTimestep
 //! scheduling features currently offered by the Bevy game engine.
-//! 
+//!
 //! The ones provided by this crate do not use "looping stages", and can therefore
 //! be combined/composed together elegantly, solving some of the most annoying
 //! usability limitations of the respective APIs in Bevy.
@@ -17,19 +17,21 @@ pub mod state;
 
 /// Prelude: convenient import for all the user-facing APIs provided by the crate
 pub mod prelude {
-    pub use crate::condition::{ConditionHelpers, IntoConditionalSystem, ConditionSet, AddConditionalToSet};
+    pub use crate::condition::{
+        AddConditionalToSet, ConditionHelpers, ConditionSet, IntoConditionalSystem,
+    };
 
-    #[cfg(feature = "fixedtimestep")]
-    pub use crate::fixedtimestep::{FixedTimesteps, FixedTimestepStage};
-    #[cfg(feature = "fixedtimestep")]
-    pub use crate::fixedtimestep::schedule::ScheduleLooplessFixedTimestepExt;
     #[cfg(all(feature = "fixedtimestep", feature = "app"))]
     pub use crate::fixedtimestep::app::AppLooplessFixedTimestepExt;
+    #[cfg(feature = "fixedtimestep")]
+    pub use crate::fixedtimestep::schedule::ScheduleLooplessFixedTimestepExt;
+    #[cfg(feature = "fixedtimestep")]
+    pub use crate::fixedtimestep::{FixedTimestepStage, FixedTimesteps};
 
-    #[cfg(feature = "states")]
-    pub use crate::state::{CurrentState, NextState, StateTransitionStage};
-    #[cfg(feature = "states")]
-    pub use crate::state::schedule::ScheduleLooplessStateExt;
     #[cfg(all(feature = "states", feature = "app"))]
     pub use crate::state::app::AppLooplessStateExt;
+    #[cfg(feature = "states")]
+    pub use crate::state::schedule::ScheduleLooplessStateExt;
+    #[cfg(feature = "states")]
+    pub use crate::state::{CurrentState, NextState, StateTransitionStage, WaitingState};
 }
